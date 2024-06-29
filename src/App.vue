f824f7b10d8abde63140052f44844d0c251b2602
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-30 00:00:32 +0200

    feat: new project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+    components: { 
+      AppMain,
+    }
+};
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <h1>App Main</h1>
+</template>
+
+<style scoped lang="scss">
+</style>
