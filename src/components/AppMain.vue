dd95ecdb55978b114496cae1b28f1b6b93e3bc23
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-30 00:15:32 +0200

    feat: created in AppMain and print cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,32 @@
 <script>
+import axios from 'axios';
 export default {
-
-}
+    data() {
+        return {
+            projects: []
+        }
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
+            this.projects = resp.data.results
+        });
+    }
+};
 </script>
 
 <template>
-    <h1>App Main</h1>
+    <div class="container">
+        <h1 class="mb-4 mt-4">PROGETTI:</h1>
+        <div class="row row-cols-4 g-4 mt-4">
+            <div class="col" v-for="project in projects">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>{{ project.title }}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
