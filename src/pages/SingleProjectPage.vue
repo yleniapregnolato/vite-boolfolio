46a8d9d595c68e6a724153fa9a499e3dcf444d3f
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-04 09:32:04 +0200

    feat: technologies in SingleProjectPage

--- src/pages/SingleProjectPage.vue
+++ src/pages/SingleProjectPage.vue
@@ -12,8 +12,8 @@ export default {
         axios.get(`http://127.0.0.1:8000/api/projects/${slug}`).then(resp => {
             this.project = resp.data;
         })
-        
-        .catch((error) => {});
+
+            .catch((error) => { });
     },
 
 };
@@ -22,8 +22,14 @@ export default {
 <template>
     <div class="container" v-if="project">
         <h2>{{ project.title }}</h2>
+        <p>{{ project.content }}</p>
+
+        <h3>Tecnologie utilizzate:</h3>
+        <ul v-for="technology in project.technologies">
+            <li>{{ technology.name }}</li>
+            <p>{{ technology.description }}</p>
+        </ul>
     </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
